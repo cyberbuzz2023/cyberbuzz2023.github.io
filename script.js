33aeb868921b93ebc7d5deda510e8c26d40dc014
cyberbuz2021 <cyberbuzz.amity@gmail.com>
2023-09-21 01:31:27 +0530

    Done

--- script.js
+++ script.js
@@ -1,9 +1,14 @@
 function closeModal(modalId) {
+    console.log("Closing modal: " + modalId); 
     var modal = document.getElementById(modalId);
     if (modal) {
         modal.style.display = "none";
     }
 }
+
+
+
+
 
 window.onload = function () {
     // Get all elements with class "box"
@@ -23,20 +28,28 @@ window.onload = function () {
         });
     });
 
+    // Function to close the modal by modal ID
+    function closeModal(modalId) {
+        var modal = document.getElementById(modalId);
+        if (modal) {
+            modal.style.display = "none";
+        }
+    }
+
     // Add a click event listener to close buttons within modals
     var closeButtons = document.querySelectorAll(".close-button");
     closeButtons.forEach(function (closeButton) {
         closeButton.addEventListener("click", function () {
-            // Get the modal ID from the data-modal attribute
-            var modalId = closeButton.getAttribute("data-modal");
+            // Get the modal ID from the parent modal
+            var modalId = closeButton.parentNode.parentNode.id;
             closeModal(modalId);
         });
     });
 
-    // Add event delegation for closing modals when clicking outside
-    document.body.addEventListener("click", function (event) {
+    // Add a click event listener to close modals when clicking outside
+    window.onclick = function (event) {
         if (event.target.classList.contains("modal")) {
             event.target.style.display = "none";
         }
-    });
+    };
 };
