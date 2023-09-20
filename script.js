385f791925f51eba39f68d8ae0b6ef286edfbe8b
cyberbuz2021 <cyberbuzz.amity@gmail.com>
2023-09-21 00:33:49 +0530

    DOne

--- script.js
+++ script.js
@@ -1,184 +1,43 @@
-window.onload = function(){
+window.onload = function () {
+    // Get all elements with class "box"
+    var boxes = document.querySelectorAll(".box");
 
+    // Add a click event listener to each box
+    boxes.forEach(function (box) {
+        box.addEventListener("click", function () {
+            // Get the data-modal attribute value
+            var modalId = box.getAttribute("data-modal");
 
-  document.getElementById("hv-1").onclick = function() {
-      document.getElementById('myModal-01').style.display = "block";
-  }
+            // Display the corresponding modal
+            var modal = document.getElementById(modalId);
+            if (modal) {
+                modal.style.display = "block";
+            }
+        });
+    });
 
-  document.getElementById("hv-2").onclick = function() {
-      document.getElementById('myModal-02').style.display = "block";
-  }
-
-  document.getElementById("hv-3").onclick = function() {
-     document.getElementById('myModal-03').style.display = "block";
-  }
-
-  document.getElementById("hv-4").onclick = function() {
-      document.getElementById('myModal-04').style.display = "block";
-  }
-
-  document.getElementById("hv-5").onclick = function() {
-      document.getElementById('myModal-05').style.display = "block";
-  }
-
-  document.getElementById("hv-6").onclick = function() {
-      document.getElementById('myModal-06').style.display = "block";
-  }
-
-  document.getElementById("hv-7").onclick = function() {
-      document.getElementById('myModal-07').style.display = "block";
-  }
-
-  document.getElementById("hv-8").onclick = function() {
-      document.getElementById('myModal-08').style.display = "block";
-  }
-
-  document.getElementById("hv-9").onclick = function() {
-      document.getElementById('myModal-09').style.display = "block";
-  }
-
-  document.getElementById("hv-10").onclick = function() {
-      document.getElementById('myModal-10').style.display = "block";
-  }
-
-  document.getElementById("hv-11").onclick = function() {
-      document.getElementById('myModal-11').style.display = "block";
-  }
-
-  document.getElementById("hv-12").onclick = function() {
-      document.getElementById('myModal-12').style.display = "block";
-  }
-
-  document.getElementById("hv-13").onclick = function() {
-      document.getElementById('myModal-13').style.display = "block";
-  }
-
-  document.getElementById("hv-14").onclick = function() {
-      document.getElementById('myModal-14').style.display = "block";
-  }
-
-
-
-
-window.onclick = function(event) {
-    if (event.target == document.getElementById('myModal-01')) {
-       document.getElementById('myModal-01').style.display = "none";
+    // Function to close the modal by modal ID
+    function closeModal(modalId) {
+        var modal = document.getElementById(modalId);
+        if (modal) {
+            modal.style.display = "none";
+        }
     }
-    else if (event.target == document.getElementById('myModal-02')) {
-       document.getElementById('myModal-02').style.display = "none";
-    }
-    else if (event.target == document.getElementById('myModal-03')) {
-       document.getElementById('myModal-03').style.display = "none";
-    }
-    else if (event.target == document.getElementById('myModal-04')) {
-       document.getElementById('myModal-04').style.display = "none";
-    }
-    else if (event.target == document.getElementById('myModal-05')) {
-       document.getElementById('myModal-05').style.display = "none";
-    }
-    else if (event.target == document.getElementById('myModal-06')) {
-        document.getElementById('myModal-06').style.display = "none";
-    }
-    else if (event.target == document.getElementById('myModal-07')) {
-       document.getElementById('myModal-07').style.display = "none";
-    }
-    else if (event.target == document.getElementById('myModal-08')) {
-       document.getElementById('myModal-08').style.display = "none";
-    }
-    else if (event.target == document.getElementById('myModal-09')) {
-       document.getElementById('myModal-09').style.display = "none";
-    }
-    else if (event.target == document.getElementById('myModal-10')) {
-       document.getElementById('myModal-10').style.display = "none";
-    }
-    else if (event.target == document.getElementById('myModal-11')) {
-       document.getElementById('myModal-11').style.display = "none";
-    }
-    else if (event.target == document.getElementById('myModal-12')) {
-       document.getElementById('myModal-12').style.display = "none";
-    }
-    else if (event.target == document.getElementById('myModal-13')) {
-       document.getElementById('myModal-13').style.display = "none";
-    }
-    else if (event.target == document.getElementById('myModal-14')) {
-       document.getElementById('myModal-14').style.display = "none";
-    }
-}
 
-}
+    // Add a click event listener to close buttons within modals
+    var closeButtons = document.querySelectorAll(".close-button");
+    closeButtons.forEach(function (closeButton) {
+        closeButton.addEventListener("click", function () {
+            // Get the modal ID from the parent modal
+            var modalId = closeButton.parentNode.parentNode.id;
+            closeModal(modalId);
+        });
+    });
 
-function closeModal() {
-    var modal1 = document.getElementById("myModal-01");
-    var modal2 = document.getElementById("myModal-02");
-    var modal3 = document.getElementById("myModal-03");
-    var modal4 = document.getElementById("myModal-04");
-    var modal5 = document.getElementById("myModal-05");
-    var modal6 = document.getElementById("myModal-06");
-    var modal7 = document.getElementById("myModal-07");
-    var modal8 = document.getElementById("myModal-08");
-    var modal9 = document.getElementById("myModal-09");
-    var modal10 = document.getElementById("myModal-10");
-    modal1.style.display = "none";
-    modal2.style.display = "none";
-    modal3.style.display = "none";
-    modal4.style.display = "none";
-    modal5.style.display = "none";
-    modal6.style.display = "none";
-    modal7.style.display = "none";
-    modal8.style.display = "none";
-    modal9.style.display = "none";
-    modal10.style.display = "none";
-}
-
-
-
-var box = document.getElementById("hv-1"); 
-var modal = document.getElementById("myModal-01"); 
-box.addEventListener("click", function () {
-    modal.style.display = "block";
-});
-var box = document.getElementById("hv-2"); 
-var modal = document.getElementById("myModal-02"); 
-box.addEventListener("click", function () {
-    modal.style.display = "block";
-});
-var box = document.getElementById("hv-3"); 
-var modal = document.getElementById("myModal-03"); 
-box.addEventListener("click", function () {
-    modal.style.display = "block";
-});
-var box = document.getElementById("hv-4"); 
-var modal = document.getElementById("myModal-04"); 
-box.addEventListener("click", function () {
-    modal.style.display = "block";
-});
-var box = document.getElementById("hv-5"); 
-var modal = document.getElementById("myModal-05"); 
-box.addEventListener("click", function () {
-    modal.style.display = "block";
-});
-var box = document.getElementById("hv-6"); 
-var modal = document.getElementById("myModal-06"); 
-box.addEventListener("click", function () {
-    modal.style.display = "block";
-});
-var box = document.getElementById("hv-7"); 
-var modal = document.getElementById("myModal-07"); 
-box.addEventListener("click", function () {
-    modal.style.display = "block";
-});
-var box = document.getElementById("hv-8"); 
-var modal = document.getElementById("myModal-08"); 
-box.addEventListener("click", function () {
-    modal.style.display = "block";
-});
-var box = document.getElementById("hv-9"); 
-var modal = document.getElementById("myModal-09"); 
-box.addEventListener("click", function () {
-    modal.style.display = "block";
-});
-var box = document.getElementById("hv-10"); 
-var modal = document.getElementById("myModal-10"); 
-box.addEventListener("click", function () {
-    modal.style.display = "block";
-});
+    // Add a click event listener to close modals when clicking outside
+    window.onclick = function (event) {
+        if (event.target.classList.contains("modal")) {
+            event.target.style.display = "none";
+        }
+    };
+};
