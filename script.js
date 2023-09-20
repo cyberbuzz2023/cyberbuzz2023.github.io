443810eadf4d024af0a745eadeb1079e46ec8777
cyberbuz2021 <cyberbuzz.amity@gmail.com>
2023-09-21 01:17:14 +0530

    Done

--- script.js
+++ script.js
@@ -1,11 +1,9 @@
-
 function closeModal(modalId) {
     var modal = document.getElementById(modalId);
     if (modal) {
         modal.style.display = "none";
     }
 }
-
 
 window.onload = function () {
     // Get all elements with class "box"
@@ -25,21 +23,20 @@ window.onload = function () {
         });
     });
 
-
     // Add a click event listener to close buttons within modals
     var closeButtons = document.querySelectorAll(".close-button");
     closeButtons.forEach(function (closeButton) {
         closeButton.addEventListener("click", function () {
-            // Get the modal ID from the parent modal
-            var modalId = closeButton.parentNode.parentNode.id;
+            // Get the modal ID from the data-modal attribute
+            var modalId = closeButton.getAttribute("data-modal");
             closeModal(modalId);
         });
     });
 
-    // Add a click event listener to close modals when clicking outside
-    window.onclick = function (event) {
+    // Add event delegation for closing modals when clicking outside
+    document.body.addEventListener("click", function (event) {
         if (event.target.classList.contains("modal")) {
             event.target.style.display = "none";
         }
-    };
+    });
 };
