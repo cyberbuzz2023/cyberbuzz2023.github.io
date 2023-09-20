dde58d5e7b95a5920995c998e98ede7d017875a7
cyberbuz2021 <cyberbuzz.amity@gmail.com>
2023-09-20 23:46:08 +0530

    Done

--- script.js
+++ script.js
@@ -106,3 +106,79 @@ window.onclick = function(event) {
 }
 
 }
+
+function closeModal() {
+    var modal1 = document.getElementById("myModal-01");
+    var modal2 = document.getElementById("myModal-02");
+    var modal3 = document.getElementById("myModal-03");
+    var modal4 = document.getElementById("myModal-04");
+    var modal5 = document.getElementById("myModal-05");
+    var modal6 = document.getElementById("myModal-06");
+    var modal7 = document.getElementById("myModal-07");
+    var modal8 = document.getElementById("myModal-08");
+    var modal9 = document.getElementById("myModal-09");
+    var modal10 = document.getElementById("myModal-10");
+    modal1.style.display = "none";
+    modal2.style.display = "none";
+    modal3.style.display = "none";
+    modal4.style.display = "none";
+    modal5.style.display = "none";
+    modal6.style.display = "none";
+    modal7.style.display = "none";
+    modal8.style.display = "none";
+    modal9.style.display = "none";
+    modal10.style.display = "none";
+}
+
+
+
+var box = document.getElementById("hv-1"); 
+var modal = document.getElementById("myModal-01"); 
+box.addEventListener("click", function () {
+    modal.style.display = "block";
+});
+var box = document.getElementById("hv-2"); 
+var modal = document.getElementById("myModal-02"); 
+box.addEventListener("click", function () {
+    modal.style.display = "block";
+});
+var box = document.getElementById("hv-3"); 
+var modal = document.getElementById("myModal-03"); 
+box.addEventListener("click", function () {
+    modal.style.display = "block";
+});
+var box = document.getElementById("hv-4"); 
+var modal = document.getElementById("myModal-04"); 
+box.addEventListener("click", function () {
+    modal.style.display = "block";
+});
+var box = document.getElementById("hv-5"); 
+var modal = document.getElementById("myModal-05"); 
+box.addEventListener("click", function () {
+    modal.style.display = "block";
+});
+var box = document.getElementById("hv-6"); 
+var modal = document.getElementById("myModal-06"); 
+box.addEventListener("click", function () {
+    modal.style.display = "block";
+});
+var box = document.getElementById("hv-7"); 
+var modal = document.getElementById("myModal-07"); 
+box.addEventListener("click", function () {
+    modal.style.display = "block";
+});
+var box = document.getElementById("hv-8"); 
+var modal = document.getElementById("myModal-08"); 
+box.addEventListener("click", function () {
+    modal.style.display = "block";
+});
+var box = document.getElementById("hv-9"); 
+var modal = document.getElementById("myModal-09"); 
+box.addEventListener("click", function () {
+    modal.style.display = "block";
+});
+var box = document.getElementById("hv-10"); 
+var modal = document.getElementById("myModal-10"); 
+box.addEventListener("click", function () {
+    modal.style.display = "block";
+});
